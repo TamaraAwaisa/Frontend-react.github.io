044e0f28ca583866142697f30190473606ca6b7e
TamaraAwaisa <tamaraawaisa3@gmail.com>
2024-01-08 21:45:55 +0200

    refactor the code to use the endpoint

--- react-vite-project/src/components/CardsTypes/CardsTypes.jsx
+++ react-vite-project/src/components/CardsTypes/CardsTypes.jsx
@@ -1,10 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
 import { Box } from '@mui/material';
 import '@fontsource/sevillana';
 import Cards from '../Cards/Cards';
-import Data1 from '../Data/Data1';
-import Data2 from '../Data/Data2';
-import Data3 from '../Data/Data3';
 import { styled } from '@mui/system';
+import { allData, dataType } from '../../mocks/data';
 
 const StyledTitle = styled('h5')({
     fontFamily: 'Sevillana, sans-serif',
@@ -14,35 +13,59 @@ const StyledTitle = styled('h5')({
     marginBottom: '80px',
 });
 
-
-const Title1 = 'Flower Bouquets';
-const Title2 = 'Flower Baskets';
-const Title3 = 'Flower Box';
+const useMockDataQuery = (dataTypeKey) => {
+    return useQuery({
+        queryKey:['allData', dataTypeKey],
+        queryFn: async () => {
+            try {
+                const data = allData[dataTypeKey];
+                if (!data) {
+                    throw new Error(`Data type not found: ${dataTypeKey}`);
+                }
+                return data;
+            } catch (error) {
+                throw new Error(`Error fetching data: ${error.message}`);
+            }
+        },
+    });
+};
 
 function CardsTypes() {
+    const {
+        data: bouquetData
+    } = useMockDataQuery(dataType.bouquet);
+
+    const {
+        data: basketData
+    } = useMockDataQuery(dataType.basket);
+
+    const { data: boxData
+    } = useMockDataQuery(dataType.box);
+
+
+    if (!bouquetData || !basketData || !boxData) {
+        return <p>Data not available</p>;
+    }
+
     return (
         <>
-        <Box position="static" sx={{ flexGrow: 1 }}>
-            <StyledTitle>
-                {Title1}
-            </StyledTitle>
-            <Cards Data={Data1}/>
-        </Box>
+            <Box position="static" sx={{ flexGrow: 1 }}>
+                <StyledTitle>{bouquetData.title}</StyledTitle>
+                <Cards Data={bouquetData.data} />
+            </Box>
 
-        <Box position="static" sx={{ flexGrow: 1 }}>
-            <StyledTitle>
-                {Title2}
-            </StyledTitle>
-            <Cards Data={Data2}/>
-        </Box>
+            <Box position="static" sx={{ flexGrow: 1 }}>
+                <StyledTitle>{basketData.title}</StyledTitle>
+                <Cards Data={basketData.data} />
+            </Box>
 
-        <Box position="static" sx={{ flexGrow: 1 }}>
-            <StyledTitle>
-                {Title3}
-            </StyledTitle>
-            <Cards Data={Data3}/>
-        </Box>
+            <Box position="static" sx={{ flexGrow: 1 }}>
+                <StyledTitle>{boxData.title}</StyledTitle>
+                <Cards Data={boxData.data} />
+            </Box>
         </>
-    )
+    );
 }
+
 export default CardsTypes;
+
